c848882573f1ef54168a1bd591ca9d85e326efcf
Nakamo312 <shehmatov.vitek@gmail.com>
2025-12-05 11:08:32 +0300

    update reference

--- Clinic/Clinic.RabbitMq.Consumer/Program.cs
+++ Clinic/Clinic.RabbitMq.Consumer/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
+using Clinic.RabbitMq.Consumer.Services;
 using Clinic.Application.Profiles;
 using Clinic.ServiceDefaults; 
 
